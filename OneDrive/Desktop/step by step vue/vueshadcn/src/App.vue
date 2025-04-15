04f0b429c998a121d1f8acacf10c9c98ad1733dd
De Gonzague <comoenoelmariefrancois@gmail.com>
2025-04-15 16:24:48 +0000

    failed

--- OneDrive/Desktop/step by step vue/vueshadcn/src/App.vue
+++ OneDrive/Desktop/step by step vue/vueshadcn/src/App.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <button
@@ -49,6 +62,27 @@
   >
     Purple
   </button>
+  <Button variant="ghost">Click me</Button>
+  <AlertDialog>
+    <AlertDialogTrigger as-child>
+      <Button variant="outline">
+        Show Dialog
+      </Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
 </template>
 
 <style scoped></style>
--- OneDrive/Desktop/step by step vue/vueshadcn/vite.config.ts
+++ OneDrive/Desktop/step by step vue/vueshadcn/vite.config.ts
@@ -1,19 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
